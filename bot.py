70d81b12d56fc546d100e687d0dc4fa5225f51bc
Ethan Cheng <ethanrc0528@gmail.com>
2021-07-06 01:15:52 -0700

    edited help command

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ async def on_message(message):
             await message.channel.send(send_list)
         
         if cmd == '-h' or cmd == '--help':
-            help_mesg = "`-h`, `--help`\n    displays help message\n`-l`, `--list`\n    lists entries in database\n    defaults to last 10 entries\n    optional arguments: OFFSET, ROWS (TODO)\n`-q`, `--query`\n    queries database for question (TODO)"
+            help_mesg = "`-h`, `--help`\n    displays help message\n`-l`, `--list`\n    lists entries in database\n    defaults to last 10 entries\n    optional arguments: OFFSET, ROWS (TODO)\n`-q`, `--query`\n    queries database for question (TODO)\n`-s`, `--send`\n    sends current database to chat (TODO)"
             await message.channel.send(help_mesg)
 
 
