2716862f4c646a95432a1363e570f1cd84f2c9ec
Ethan Cheng <ethanrc0528@gmail.com>
2021-07-08 13:51:47 -0700

    check integrity of database

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 guild = discord.Guild
+uid = os.getenv('id')
 
 def check_non_empty(arg):
     if len(arg.content) == 0:
@@ -51,61 +52,65 @@ async def on_message(message):
             trivia_answer = c.fetchone()
             if trivia_answer:
                 await message.channel.send(f"Answer is {trivia_answer[3]}, {trivia_answer[2]}")
-#check database for question and if string is non-empty, type answer in chat
-#else add question and answer to database
-            else:
-                msg = await client.wait_for('message', check=check, timeout=30)
-                dank = await client.wait_for('message', check=answer, timeout=30)
-                #await message.channel.send(msg.content)
-                #await message.channel.send(dank.content)
-                if dank.reference.message_id == msg.id:
-                    
-                    
-                    #await message.channel.send("question: " + question)
 
-                    option_a = str(descr[3]).replace('A) ', '').replace('*', '')
-                    option_b = str(descr[4]).replace('B) ', '').replace('*', '')
-                    option_c = str(descr[5]).replace('C) ', '').replace('*', '')
-                    option_d = str(descr[6]).replace('D) ', '').replace('*', '')
-                    options = option_a + '\n'+ option_b + '\n'+ option_c + '\n'+ option_d
-                    #await message.channel.send("options: " + options) 
-                    
-                    letter = ''
-                    correct_answer = ''
+            msg = await client.wait_for('message', check=check, timeout=16)
+            dank = await client.wait_for('message', check=answer, timeout=16)
+            #await message.channel.send(msg.content)
+            #await message.channel.send(dank.content)
+            if dank.reference.message_id == msg.id:
+                
+                
+                #await message.channel.send("question: " + question)
 
-                    if 'coin' in str(dank.content):
-                        letter = str(msg.content).upper()
-                        if letter == 'A':
-                            correct_answer = option_a
-                        elif letter == 'B':
-                            correct_answer = option_b
-                        elif letter == 'C':
-                            correct_answer = option_c
-                        elif letter == 'D':
-                            correct_answer = option_d
-                        else:
-                            await message.channel.send("error")
-                            return
-                    elif 'no' in str(dank.content):
-                        correct_answer = str(dank.content).split('`')[1]
-                        if correct_answer == option_a:
-                            letter = 'A'
-                        elif correct_answer == option_b:
-                            letter = 'B'
-                        elif correct_answer == option_c:
-                            letter = 'C'
-                        elif correct_answer == option_d:
-                            letter = 'D'
-                        else:
-                            await message.channel.send("error, not committing to database")
-                            return
+                option_a = str(descr[3]).replace('A) ', '').replace('*', '')
+                option_b = str(descr[4]).replace('B) ', '').replace('*', '')
+                option_c = str(descr[5]).replace('C) ', '').replace('*', '')
+                option_d = str(descr[6]).replace('D) ', '').replace('*', '')
+                options = option_a + '\n'+ option_b + '\n'+ option_c + '\n'+ option_d
+                #await message.channel.send("options: " + options) 
+                
+                letter = ''
+                correct_answer = ''
+
+                if 'coin' in str(dank.content):
+                    letter = str(msg.content).upper()
+                    if letter == 'A':
+                        correct_answer = option_a
+                    elif letter == 'B':
+                        correct_answer = option_b
+                    elif letter == 'C':
+                        correct_answer = option_c
+                    elif letter == 'D':
+                        correct_answer = option_d
                     else:
-                        await message.channel.send("error parsing dankmemer response, not committing answer to database")
-                        return None
+                        await message.channel.send("error")
+                        return
+                elif 'no' in str(dank.content):
+                    correct_answer = str(dank.content).split('`')[1]
+                    if correct_answer == option_a:
+                        letter = 'A'
+                    elif correct_answer == option_b:
+                        letter = 'B'
+                    elif correct_answer == option_c:
+                        letter = 'C'
+                    elif correct_answer == option_d:
+                        letter = 'D'
+                    else:
+                        await message.channel.send("error, not committing to database")
+                        return
+                else:
+                    await message.channel.send("error parsing dankmemer response, not committing answer to database")
+                    return
 
-                    #await message.channel.send("answer: " + correct_answer)
-                    #await message.channel.send("letter: " + letter)
+                #await message.channel.send("answer: " + correct_answer)
+                #await message.channel.send("letter: " + letter)
 
+                if trivia_answer:
+                    if trivia_answer[1] == options and trivia_answer[2] == correct_answer and trivia_answer[3] == letter:
+                        await message.channel.send("database is consistent")
+                    else:
+                        await message.channel.send(f"<@{uid}>, discrepancy detected between database and answer")
+                else:
                     c.execute("INSERT INTO trivia_answers (Question, Options, Answer, Letter) VALUES (?, ?, ?, ?)", (question, options, correct_answer, letter))
                     conn.commit()
 
