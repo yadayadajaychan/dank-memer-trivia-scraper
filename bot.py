92a980e22d8d02a22fdc39e06ab0afd751dc5ff3
Ethan Cheng <ethanrc0528@gmail.com>
2021-08-09 15:51:32 -0700

    changed message which displays answer

--- bot.py
+++ bot.py
@@ -51,7 +51,7 @@ async def on_message(message):
             c.execute("SELECT * FROM trivia_answers WHERE Question=(?)", (question,))
             trivia_answer = c.fetchone()
             if trivia_answer:
-                await message.channel.send(f"Answer is {trivia_answer[2]}")
+                await message.channel.send(f"Answer is `{trivia_answer[2]}`")
                 open("answer.txt", "w").write(trivia_answer[3])
 
             msg = await client.wait_for('message', check=check, timeout=16)
